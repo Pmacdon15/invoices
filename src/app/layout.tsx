c246c8b6133b8cc1cdc9611bfad0273adce487ec
Patrick MacDonald <patrick@patmac.ca>
2026-02-23 14:48:33 -0700

    added auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
+import { Navbar } from "@/components/navbar";
 import Providers from "@/components/providers/provider";
 
 const geistSans = Geist({
@@ -13,9 +16,6 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-import { Navbar } from "@/components/navbar";
-import { Toaster } from "sonner";
-
 export const metadata: Metadata = {
   title: "InvoicePro - Effortless Invoicing",
   description: "Manage customers, products, and invoices with ease.",
@@ -27,25 +27,27 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen bg-background text-foreground`}
-      >
-        <Providers>
-          <div className="relative flex min-h-screen flex-col">
-            <Navbar />
-            <main className="flex-1">{children}</main>
-            <Toaster />
-            <footer className="border-t py-6 md:px-8 md:py-0">
-              <div className="container mx-auto flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row px-4">
-                <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
-                  Built for professionals. Patrick Macdonald @ 2026.
-                </p>
-              </div>
-            </footer>
-          </div>
-        </Providers>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen bg-background text-foreground`}
+        >
+          <Providers>
+            <div className="relative flex min-h-screen flex-col">
+              <Navbar />
+              <main className="flex-1">{children}</main>
+              <Toaster />
+              <footer className="border-t py-6 md:px-8 md:py-0">
+                <div className="container mx-auto flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row px-4">
+                  <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
+                    Built for professionals. Patrick Macdonald @ 2026.
+                  </p>
+                </div>
+              </footer>
+            </div>
+          </Providers>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
